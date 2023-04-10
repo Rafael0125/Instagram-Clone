ec67c15162e40e68485290cff7547fafba4a953e
RAFAEL NAVARRO <rafanavarro0125@gmail.com>
2023-04-10 09:12:14 -0300

    Criado os componentes acesso,banner,login e cadastro

--- app3/src/app/app.module.ts
+++ app3/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AcessoComponent } from './acesso/acesso.component';
+import { BannerComponent } from './acesso/banner/banner.component';
+import { LoginComponent } from './acesso/login/login.component';
+import { CadastroComponent } from './acesso/cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AcessoComponent
+    AcessoComponent,
+    BannerComponent,
+    LoginComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
---
+++ passo-a-passo/moduloX/aulasX.md
@@ -0,0 +1,13 @@
+Modulo 24 #App3(instagram-clone)-Primeiro Passos
+    Aula 285 - Criei os componentes acesso, banner, login e cadastro
+
+    
+
+
+
+
+
+
+
+
+
