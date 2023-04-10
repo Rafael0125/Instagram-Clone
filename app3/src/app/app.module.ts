1b83c9db52e2876115293099d0ac4a0cf2314fc2
RAFAEL NAVARRO <rafanavarro0125@gmail.com>
2023-04-10 09:05:46 -0300

    Configurado o bootstrap e criado o componente acesso

--- app3/src/app/app.module.ts
+++ app3/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AcessoComponent } from './acesso/acesso.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcessoComponent
   ],
   imports: [
     BrowserModule,
---
+++ passo-a-passo/instalacao-do-ambiente.md
@@ -0,0 +1,7 @@
+Iniciei o projeto instalando o bootstrap 5 com o jquery e o fontawesome
+
+    npm install --save bootstrap /// Bootstrap 5
+    npm install --save jquery /// Jquery
+    npm install --save @fortawesome/fontawesome-free /// Fontawesome
+
+    O @popper ja vem com a instalação do bootstrap 5 
